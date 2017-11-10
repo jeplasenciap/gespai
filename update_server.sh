30a96be4f13f94e7d60b6dc4840b236302b88ada
JESUS EDUARDO PLASENCIA PIMENTEL <alu0100791327@djangoapps01.osl.ull.es>
2017-11-10 10:18:27 +0000

    hacer cambios en configuración para usar mysql
    - se ha ajustado la raíz del servidor
    - se ha configurado mysql como el sistema gestor de BBDD

--- update_server.sh
+++ update_server.sh
@@ -1,1 +1,1 @@
-mod_wsgi-express setup-server --user www-data --group www-data --server-root /var/www/gespai --document-root /var/www/gespai/static --host 0.0.0.0 --port 80 gespai/wsgi.py
+mod_wsgi-express setup-server --user www-data --group www-data --server-root /var/www/gespai --document-root /var/www/gespai --host 0.0.0.0 --port 80 gespai/wsgi.py
